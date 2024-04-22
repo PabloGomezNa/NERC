65fb0d3dccbd823870c78b435e9c82c314f43075
Andreu Giménez Larred <andreugimenezlarred@gmail.com>
2024-04-22 12:58:44 +0200

    afegir DDI

---
+++ DDI/run.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+BASEDIR=../
+
+ #./C:/Users/andre/Downloads/stanford-corenlp-4.5.6/stanford-corenlp-4.5.6/corenlp.sh -quiet true -port 9000 -timeout 15000  &
+ #sleep 1
+
+# extract features
+echo "Extracting features"
+python extract-features.py $BASEDIR/data/devel/ > devel.cod &
+python extract-features.py $BASEDIR/data/train/ | tee train.cod | cut -f4- > train.cod.cl
+
+# kill `cat /tmp/corenlp-server.running`
+
+# train model
+echo "Training model"
+python train-sklearn.py model.joblib vectorizer.joblib < train.cod.cl
+# run model
+echo "Running model..."
+python predict-sklearn.py model.joblib vectorizer.joblib < devel.cod > devel.out
+# evaluate results
+echo "Evaluating results..."
+python evaluator.py DDI $BASEDIR/data/devel/ devel.out > devel.stats
+
